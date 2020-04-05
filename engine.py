48ef7f65a40a603f9caadaa95ace62ad223b0666
Adam <57396704+Sankti@users.noreply.github.com>
2020-04-05 02:39:42 +0200

    Update engine.py

--- engine.py
+++ engine.py
@@ -2,12 +2,16 @@ import pygame
 from pygame.locals import *
 from settings import *
 
-sprite_wall = pygame.image.load('tileset\wall.png')
-sprite_wall = pygame.transform.scale(sprite_wall, (32, 32))
-sprite_socket = pygame.image.load('tileset\socket.png')
-sprite_socket = pygame.transform.scale(sprite_socket, (32, 32))
-sprite_orb = pygame.image.load('tileset\orb.png')
-sprite_orb = pygame.transform.scale(sprite_orb, (32, 32))
+sprite_empty = pygame.image.load('empty.png')
+sprite_empty = pygame.transform.scale(sprite_empty, (TILESIZE, TILESIZE))
+sprite_wall = pygame.image.load('wall.png')
+sprite_wall = pygame.transform.scale(sprite_wall, (TILESIZE, TILESIZE))
+sprite_socket = pygame.image.load('socket.png')
+sprite_socket = pygame.transform.scale(sprite_socket, (TILESIZE, TILESIZE))
+sprite_orb_unlit = pygame.image.load('orb_unlit.png')
+sprite_orb_unlit = pygame.transform.scale(sprite_orb_unlit, (TILESIZE, TILESIZE))
+sprite_orb = pygame.image.load('orb.png')
+sprite_orb = pygame.transform.scale(sprite_orb, (TILESIZE, TILESIZE))
 
 pygame.init()
 ROOT = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -36,7 +40,44 @@ level1 = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
+def createMap(level):
+    """
+    Used to draw a level map: collection of
+    coordinates and types of tiles.
+    level - matrix, definition of a level.
+    """
+    level_map = []
+    for column in range(0, GRID_WIDTH):
+        column_raw = level[column]
+        for row in range(0, GRID_HEIGHT):
+            cell = [row, column, column_raw[row]]
+            print(cell)
+            level_map.append(cell)
+    return level_map
 
+def drawMap(level_map):
+    """
+    Used to print graphical representation
+    of a level map, using tiles.
+    level_map - matrix, result of createMap(level)
+    """
+    for tile in level_map:
+        coord_x = int(tile[0] * TILESIZE)
+        coord_y = int(tile[1] * TILESIZE)
+        print (str(tile) + "Coords: X " + str(coord_x) + " Y " + str(coord_y))
+        if tile[2] == 0:
+            ROOT.blit(sprite_empty, (coord_x, coord_y))
+        elif tile[2] == 1:
+            ROOT.blit(sprite_socket, (coord_x, coord_y))
+        elif tile[2] == 2:
+            ROOT.blit(sprite_orb_unlit, (coord_x, coord_y))
+        elif tile[2] == 3:
+            ROOT.blit(sprite_orb, (coord_x, coord_y))
+        elif tile[2] == 9:
+            ROOT.blit(sprite_wall, (coord_x, coord_y))
+
+maps = createMap(level1)
+drawMap(maps)
 
 while True:
     pygame.time.delay(100)
@@ -45,3 +86,6 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
